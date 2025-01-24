68b78f03c7acb92cf1747cb8b420b9b6e42b3aaa
Casen Ward <casenward@gmail.com>
2025-01-24 16:02:44 -0500

    Initial Commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Casen Ward
+ *        Date: 1/24/2025
+
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
